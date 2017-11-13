fa1ebe982f417f01c8b163b59e984293952671e7
Thomas Millet <thomas@mbp-de-thomas.edu.sir>
2017-11-13 14:48:10 +0100

    with gitIgnor

---
+++ first/Commentaire.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace first
+{
+    public class Commentaire
+    {
+        public Commentaire()
+        {
+            var speudo;
+            var texte;
+        }
+    }
+}
