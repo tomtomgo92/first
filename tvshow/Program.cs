730c71122d260ea086c70cb2e15e2d8c8ffc2021
Mathilde Arconte <mathilde@m-arconte.fr>
2017-11-13 17:13:03 +0100

    Reboot

--- tvshow/Program.cs
+++ tvshow/Program.cs
@@ -2,7 +2,7 @@
 
 namespace tvshow
 {
-    class Tvshow
+    class Program
     {
         static void Main(string[] args)
         {
--- tvshow/show/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace show
-{
-    public class Class1
-    {
-    }
-}
