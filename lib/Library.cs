c336e8a973d8613b2a26556f08d100c29da28b0d
Thomas Millet <thomas@mbp-de-thomas.edu.sir>
2017-11-14 12:32:23 +0100

    Add List Film and Serie

--- lib/Library.cs
+++ lib/Library.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace lib
 {
@@ -6,7 +7,33 @@ namespace lib
     {
         public Library()
         {
-            
+
+            var mediaList = new ArrayList();
+
+            mediaList.Add(new Serie{
+                Title = "Desperate",
+                Synopsis = @"
+                Desperate Housewives ou Beautés désespérées est un feuilleton 
+                télévisé américain en 180 épisodes de 42 minutes 
+                créé par Marc Cherry et diffusé entre le 3 octobre 2004 
+                et le 13 mai 2012 sur le réseau ABC et en simultané 
+                au Canada sur le réseau CTV
+                ",
+                Img = "https://str01-m.dpstream.media/s/11/desperate-housewives-55ffaf24e182e-1472989029.jpg",
+                Season = 8
+               
+                
+                });
+            mediaList.Add(new Film{
+                Title = "Interstellar",
+                Synopsis = @"
+                Interstellar, ou Interstellaire au Québec, est un film 
+                de science-fiction britannico-américain produit, 
+                écrit et réalisé par Christopher Nolan, sorti le 5 novembre 2014.
+                ",
+                Img = "http://t1.gstatic.com/images?q=tbn:ANd9GcSIVryzUKVaqs-5yb0-uHykg7Ob4rUWFl-Ojl28OvBExh8Xc8GT",
+                Director = "Christopher Nolan"
+            });
         }
     }
 }
