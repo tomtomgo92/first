4c704cc8f5c90ec490ce78fc9a7105f06f55143e
Thomas Millet <thomas@mbp-de-thomas.edu.sir>
2017-11-15 09:53:07 +0100

    dataBase work

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using lib;
+using Microsoft.Data.Sqlite;
 
 namespace app
 {
@@ -7,8 +8,35 @@ namespace app
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("coucou");
             var serie = new Serie();
 
+            var connectionStringBuilder = new SqliteConnectionStringBuilder
+            {
+                DataSource = "myDb.db"
+            };
+
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+
+                connection.Open();
+
+                var selectCommand = connection.CreateCommand();
+
+                selectCommand.CommandText = "SELECT * FROM library";
+
+
+                using (var reader = selectCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var message = $"{reader["ID"]} - {reader["Title"]}";
+                        Console.WriteLine(message);
+                    }
+                }
+
+            }
         }
     }
 }
