e2ea106f1bcb8ebaecc2c793b4a2bb3e2b5c4616
Thomas Millet <thomas@mbp-de-thomas.edu.sir>
2017-11-17 15:38:48 +0100

    Medialist

---
+++ Netflou/Netflou/Models/MediaList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Netflou.Models;
+using lib;
+
+namespace Netflou.Controllers
+{
+
+
+    public class MediaList
+    {
+        public MediaList()
+        {
+            Films = new List<Film>();
+            Series = new List<Serie>();
+        }
+
+        public List<Film> Films
+        {
+            get;
+            set;
+        }
+        public List<Serie> Series
+        {
+            get;
+            set;
+        }
+    }
+}
